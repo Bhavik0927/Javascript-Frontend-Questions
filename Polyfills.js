54300fef3f34f19a224b081e34c91b64554b0668
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2025-11-30 14:36:16 +0530

    Refactor myMap polyfill for improved readability and consistency

--- Polyfills.js
+++ Polyfills.js
@@ -1,18 +1,25 @@
-
 // Create Map Polyfill
 
-if(!Array.prototype.myMap){
-    Array.prototype.myMap = function(callback, thisArg){
-        if(typeof callback !== 'function'){
-            throw new TypeError(callback + 'is not a function')
-        }
+if (!Array.prototype.myMap) {
+  Array.prototype.myMap = function (callback, thisArg) {
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + "is not a function");
     }
 
     let result = [];
 
-    for(let i = 0; i < this.length; i++){
-        result.push(callback.call(thisArg, this[i], i, this))
+    for (let i = 0; i < this.length; i++) {
+      result.push(callback.call(thisArg, this[i], i, this));
     }
 
     return result;
+  };
 }
+
+
+
+const arr = [12,232,4,5,2222,9];
+
+const newArr = arr.myMap(arr);
+
+console.log(newArr);
