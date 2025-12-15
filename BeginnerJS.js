e5b258210e9d1d0af1febce5e381a9b52a72488a
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2025-12-15 18:39:59 +0530

    feat: add initial implementation of BeginnerJS with basic arithmetic and even/odd check

---
+++ BeginnerJS.js
@@ -0,0 +1,10 @@
+// 1)
+let a = '5';
+let b = 23;
+
+console.log(a + b);  // Its concatinate
+console.log(a - b); // Its substract
+
+// 2)
+const check = n => (n % 2 === 0) ? "Even" : "Odd";
+// console.log(check(8));
