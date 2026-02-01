d852b192f6a095dd29f163a56ce4363bae0076f6
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2026-02-01 13:42:28 +0530

    feat: add implementations of Non-Primitive and Primitive data types examples

---
+++ DataTypes/Non-Primitive.js
@@ -0,0 +1,29 @@
+// Non-primitve types are mutable and stored by reference
+
+let user = {
+    name: "Bhavik",
+    age: 28
+}
+
+user.name = "Rajbbb...";
+
+// console.log(user);
+
+
+let arr = [1,4,23];
+arr.push(1222);
+// console.log(arr);
+
+
+function greet(){
+    return "Hello...";
+}
+
+greet();
+
+
+// console.log(10 === 10)
+
+// console.log({} === {});
+
+
---
+++ DataTypes/Primitive.js
@@ -0,0 +1,26 @@
+// Primitive data types are immutable and stored by value
+
+let name = "Bhavik";
+name[0] = "b";
+// console.log(name);
+
+let x ;
+console.log(x);
+
+
+let user = null;
+console.log(typeof(user))
+
+let a = 10;
+let b = a;
+
+b = 20;
+console.log(a);
+console.log(b);
+
+
+let str = "Hii";
+console.log(str);
+
+str = str + "JS";
+console.log(str);
