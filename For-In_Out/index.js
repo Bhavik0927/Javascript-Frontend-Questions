28cda1555352dd0ddf995c535ca0c44fcaf9a5d4
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2026-02-03 21:29:43 +0530

    feat: add initial implementation of various iteration methods for objects and arrays

---
+++ For-In_Out/index.js
@@ -0,0 +1,90 @@
+/*
+const user = {
+    Name: "Bhavik",
+    City:"Nagpur"
+}
+
+for(let key in user){
+    console.log(key,user[key])
+}
+
+*/
+
+
+/*
+Object.prototype.role = 'admin';
+
+const user = {name: "Bhavik"};
+
+for(let us in user){
+    console.log(us , user[us]);
+}
+
+*/
+
+
+/*
+Object.prototype.role = "admin";
+
+const user = { name: "Bhavik" };
+
+for(let key in user){
+    if(user.hasOwnProperty(key)){
+        console.log(key);
+    }
+}
+
+*/
+
+
+/*
+const arr = [10,31,31];
+
+// for(let i in arr){
+//     console.log(arr[i]);
+// }
+
+for(let n of arr){
+    console.log(n);
+}
+
+*/
+
+
+/*
+for(let char of "Bha02vik"){
+    console.log(char);
+}
+
+*/
+
+
+/*
+const map = new Map([
+    ["Name", "Bhavik"],
+    ["Role",  "dev"]
+]);
+
+
+for(let [key, value] of map){
+    console.log(key, value)
+}
+
+*/
+
+
+/*
+const user = { name: "Bhavik" };
+
+// for(let val of user){
+//     console.log(val);
+// }
+
+for(let key of Object.keys(user)){
+    console.log(user[key]);
+    console.log(key)
+}
+
+*/
+
+
