2d050198a442a858effe43091b4b7dc725e836c1
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2026-02-05 22:44:44 +0530

    feat: add initial implementation of object creation examples using prototypes

---
+++ Prototype/index.js
@@ -0,0 +1,46 @@
+/*
+const Person = {
+    greet(){
+        console.log('Bhavik');
+    }
+}
+
+const bhavik = Object.create(Person);
+bhavik.greet();
+
+*/
+
+
+
+/*
+function Person(name){
+    this.name = name;
+}
+
+Person.prototype.greet = function (){
+    console.log(`Hello, my name is ${this.name}`);
+}
+
+const bhavik = new Person("Bhavik");
+bhavik.greet();
+
+*/
+
+
+/*
+
+const car = {
+    wheels : 4,
+    drive(){
+        console.log("Driving...");
+    }
+}
+
+const tesla = Object.create(car);
+console.log(tesla);
+console.log(tesla.wheels);
+tesla.drive();
+
+*/
+
+
