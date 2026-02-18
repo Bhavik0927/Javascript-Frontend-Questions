563be2a75f71de6a0d1d1ee59aba7f661f521be5
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2026-02-18 15:03:51 +0530

    feat: add HTML structure and implement closure examples in JavaScript

--- Closures/index.js
+++ Closures/index.js
@@ -19,15 +19,15 @@ for(var i = 1; i<= 3; i++){
 
 
 
-function createCounter(){
-    let count = 0; 
+function createCounter() {
+    let count = 0;
 
-    return{
-        increment: function(){
+    return {
+        increment: function () {
             count++;
             console.log(count);
         },
-        decrement: function(){
+        decrement: function () {
             count--;
             console.log(count);
         }
@@ -44,3 +44,35 @@ counter.increment();
 counter.increment();
 counter.decrement()
 
+
+
+function attach() {
+    const big_Data = new Array(100000).fill('data');
+
+    document.getElementById('btn').addEventListener('click', function () {
+        console.log(big_Data.length);
+    })
+}
+
+attach();
+
+
+document.getElementById('clo_btn').addEventListener('click', varFunction);
+
+
+function varFunction() {
+    for (var i = 0; i <= 5; i++) {
+        setTimeout(() => { console.log(i) }, 1000)
+    }
+}
+
+
+document.getElementById('clo_btn').addEventListener('click', varIFEE_function);
+
+function varIFEE_function(){
+    for(var i = 0; i<= 5; i++){
+        (function(i){
+            setTimeout(() => console.log(i), 1000);
+        })(i)
+    }
+}
