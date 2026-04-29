3b77b4797c93007c219b1313f2826c9638e1fb1d
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2026-04-29 18:18:30 +0530

    feat: improve Promise.myAll implementation and add Array.prototype.My_reducer polyfill

--- Polyfills/index.js
+++ Polyfills/index.js
@@ -1,25 +1,25 @@
 
-if(!Promise.myAll){
-    Promise.myAll = function (iterable){
+if (!Promise.myAll) {
+    Promise.myAll = function (iterable) {
 
-        return new Promise((resolve,reject) =>{
+        return new Promise((resolve, reject) => {
             const results = [];
             let completed = 0;
             let index = 0;
 
-            for(let item of iterable){
+            for (let item of iterable) {
                 const currentIndex = index;
                 index++;
 
-                Promise.resolve(item).then((value) =>{
+                Promise.resolve(item).then((value) => {
                     results[currentIndex] = value;
                     completed++;
 
-                    if(completed === index){ resolve(results)}
+                    if (completed === index) { resolve(results) }
                 }).catch(reject);
             }
 
-            if(index === 0){ resolve([])}
+            if (index === 0) { resolve([]) }
         })
     }
 }
@@ -28,7 +28,7 @@ const p1 = Promise.resolve(10);
 const p2 = new Promise((res) => setTimeout(() => res(20), 1000));
 const p3 = 30;
 
-Promise.myAll([p1,p2,p3]).then(console.log).catch(console.error);
+Promise.myAll([p1, p2, p3]).then(console.log).catch(console.error);
 
 
 
@@ -36,13 +36,13 @@ Promise.myAll([p1,p2,p3]).then(console.log).catch(console.error);
 
 
 
-const arr = [12,4,45,6,777,23];
+const arr = [12, 4, 45, 6, 777, 23];
 
-arr.prototype.myMap = function(callback){
+arr.prototype.myMap = function (callback) {
     const result = [];
 
-    for(let i = 0; i< this.length; i++){
-        result.push(callback(this[i], i , this))
+    for (let i = 0; i < this.length; i++) {
+        result.push(callback(this[i], i, this))
     }
     return result;
 }
@@ -51,22 +51,55 @@ arr.prototype.myMap = function(callback){
 
 // filter Polyfills
 
-if(!Array.prototype.myFilter){
-    Array.prototype.myFilter = function(cb, thisArg){
-        if(typeof cb !== "function"){
+if (!Array.prototype.myFilter) {
+    Array.prototype.myFilter = function (cb, thisArg) {
+        if (typeof cb !== "function") {
             throw new TypeError("Callback is not present");
         }
 
         let result = [];
-        for(let i = 0; i< this.length; i++){
+        for (let i = 0; i < this.length; i++) {
 
-            if(i in this){
-                if(cb.call(thisArg, this[i], i, this))
-                result.push(this[i]);
+            if (i in this) {
+                if (cb.call(thisArg, this[i], i, this)) {
+                    result.push(this[i]);
+                }
+
             }
-            
+
         }
 
         return result;
+    }
+}
+
+
+
+// Reducer Polyfill
+
+if(!Array.prototype.My_reducer){
+    Array.prototype.My_reducer = function(cb, intialValue){
+        if(typeof cb === 'function'){
+            throw new TypeError("It's not an function...!!")
+        }
+
+        let hasInitialValue = arguments.length > 0;
+
+        if(!hasInitialValue && this.length === 0){
+            throw new TypeError("It's not be a empty value");
+        }
+
+        let init_value = hasInitialValue ? intialValue : this[0];
+        let start_value = hasInitialValue ? 0 : 1;
+
+        let accumulator = init_value;
+
+        for(let i = start_value; i< this.length; i++){
+            if(i in this){
+                accumulator = cb(accumulator, this[i], i, this)
+            }
+        }
+
+        return accumulator;
     }
 }
